3f7e7a84ca1f0f5c49a9138e1516d498332eadb4
Fairchild Davis <fmd@live.unc.edu>
2023-01-21 20:56:33 -0500

    fixing issues

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,4 @@
-"""Chardle - Comp 110 EX01""" 
+"""Chardle - Comp 110 EX01!""" 
 
 __author__ = "730320788"
 
@@ -43,15 +43,10 @@ if number == 0:
 if number == 1:
     print("1 instance of " + letter + " found in " + word)
 if number == 2:
-    print("2 instance of " + letter + " found in " + word)
+    print("2 instances of " + letter + " found in " + word)
 if number == 3:
-    print("3 instance of " + letter + " found in " + word)
+    print("3 instances of " + letter + " found in " + word)
 if number == 4:
-    print("4 instance of " + letter + " found in " + word)
+    print("4 instances of " + letter + " found in " + word)
 if number == 5:
-    print("5 instance of " + letter + " found in " + word)
-
-
-
-
-
+    print("5 instances of " + letter + " found in " + word)
